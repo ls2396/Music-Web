b0ad21f4fbf9755e416057a8ef58af4b9fceffc3
lisen@Mac <lisenmac@lisenMacBook-Pro.local>
2025-02-26 19:03:57 +0900

    step1:提交代码，创建仓库

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home';
+import Songs from './pages/Songs';
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Music Website</h1>
+      <Home />
+      <Songs />
+    </div>
+  )
+}
+
+export default App
