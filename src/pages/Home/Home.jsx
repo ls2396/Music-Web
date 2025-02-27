1193fea3acd325bb62a2c8959f4310f27959309f
lisen@Mac <lisenmac@lisenMacBook-Pro.local>
2025-02-27 20:37:14 +0900

    bg-images

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import './Home.css';
 import '../../styles/global.css';
+import { useEffect } from 'react';
+
 
 function Home() {
+
+    useEffect(() => {
+        // js-动态引用
+    })
+
     return (
         <div className='home'>
             <div className='home_left'>
                 <p>L.J-Sin / Music Collection Hall</p>
-                <p>For me, as a designer—and not just a designer—music <br /> is an essential part of my life. Whenever I listen <br /> to music, I am drawn into the song by the singer's <br /> voice, immersing myself in the melody, lyrics, and <br /> everything else.</p>
+                <p>For me, as a designer—and not just a designer—music <br /> is an essential part of my life. Whenever I listen <br /> to music, I am drawn into the song by the singer is <br /> voice, immersing myself in the melody, lyrics, and <br /> everything else.</p>
                 <p>~~~~~~~~</p>
                 <p>For more of my work, please check out my portfolio: </p>
                 <a href='www.ls2396.cn' target='_blank'>www.ls2396.cn ⭕️</a>
@@ -20,112 +27,112 @@ function Home() {
                 <div className='title'>
                     <ul>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>浮誇 / U87</a>
                                 <span>2005年06月07日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>浪子回頭 / Cartoon Character</a>
                                 <span>2017年11月04日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>真夜中のドア〜stay with me / UP!</a>
                                 <span>1979年11月05日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>벌써 일년(Already One Year) / Brown Eyes</a>
                                 <span>2001年06月07日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>挪威的森林 / 愛情的盡頭</a>
                                 <span>1996年06月18日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>HUMBLE. / DAMN.</a>
                                 <span>2017年03月30日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>グッバイバイ / グッバイバイ</a>
                                 <span>2024年08月21日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>You(=i) / RED PLANET</a>
                                 <span>2016年08月29日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>Pure Water / A NEW WORLD</a>
                                 <span>2009年10月30日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>愛錯 / Shangri-La</a>
                                 <span>2004年12月31日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>Memories / Songs About Jane</a>
                                 <span>2019年09月20日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>Forever Love / Open Roa</a>
                                 <span>2008年11月26日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>Everywhere we go / 三角度</a>
                                 <span>2012年7月27日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>ドライフラワー / 壱</a>
                                 <span>2020年10月25日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>可惜夜 / Day and Night</a>
                                 <span>2024年6月8日</span>
                             </div>
                         </li>
                         <li>
-                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
+                            <h3>♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣</h3>
                             <div className='li_group'>
                                 <a href='www.ls2396.cn' target='_blank'>Slide (feat. Frank Ocean & Migos)  / Funk Wav Bounces Vol. 1</a>
                                 <span>2017年2月23日</span>
@@ -136,6 +143,7 @@ function Home() {
             </div>
             <div className=''></div>
         </div>
+
     );
 }
 
