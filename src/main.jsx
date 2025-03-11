9bf95ed57683061f80a0982930ee35cc16604a8e
lisen@Mac <lisenmac@lisenMacBook-Pro.local>
2025-03-11 16:47:06 +0900

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/index';
+import "./styles/global.css";
+import "./pages/Home/Home.css";
+import "./pages/Loading/Loading.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  base: "/Music-Web/",
+  base: "./",
   plugins: [react()],
   build: {
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
+    cssCodeSplit: false,
+    assetsInlineLimit: 0,
     rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
     },
   },
 });
